101ff47be89b44d0cbf7d8c725a441031dc54e8b
Venkat-Sumanth <sumanthbethanapalli12@gmail.com>
2024-07-09 18:57:22 +0530

    unittest unit testing

---
+++ Track/Courses/Python/Python Unit Testing/unittest Library/calc.py
@@ -0,0 +1,24 @@
+
+
+def add(x,y):
+    """Add Function"""
+    return x + y
+
+def subtract(x,y):
+    """subtract Function"""
+    return x - y
+
+def multiply(x,y):
+    """multiply Function"""
+    return x * y
+
+def divide(x,y):
+    """Divide Function"""
+    if y==0:
+        raise ValueError('can not divide by zero!')
+    return x / y
+
+# print(add(10, 5 ))
+
+
+
